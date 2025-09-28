fc6e1e27eaba3025ab093dddbc2ed1d88ee9a5a0
kpak130 <kisangpak@gmail.com>
2025-09-29 01:40:40 +0900

    Update amplitudes

--- src/components/AnxietySurvey1.tsx
+++ src/components/AnxietySurvey1.tsx
@@ -23,7 +23,7 @@ export function AnxietySurvey1({ onBack, onNext }: AnxietySurvey1Props) {
   useEffect(() => {
     // Send the new event for onboarding survey
     sendToFlutter(JSON.stringify({
-      "event": "view_ob_info_quabble_just_what_you_need",
+      "event": "view_ob_survey_anxiety_experience",
       "eventProperties": {
         "onboarding_version": 6.0
       }
